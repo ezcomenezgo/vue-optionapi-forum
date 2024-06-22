988f526b3a8f9486ef320a7e52642f7bbe9721e5
Claire Li <s3310507057@gmail.com>
2024-06-22 18:14:44 +0200

    feat: create a forum post component

---
+++ src/components/PostList.vue
@@ -0,0 +1,43 @@
+<template>
+  <div
+    class="card bg-base-300 rounded-box"
+    v-for="post in posts"
+    :key="post.id"
+  >
+    <div class="w-10 rounded-full">
+      <img :src="userById(post.userId).avatar" alt="user" />
+    </div>
+    <div class="card-body">
+      <p>
+        {{ userById(post.userId).name }}
+      </p>
+      <p>{{ post.text }}</p>
+      <p>post count: {{ userById(post.userId).postsCount }}</p>
+      <p>publish at: {{ post.publishedAt }}</p>
+    </div>
+    <div class="divider divider-accent"></div>
+  </div>
+</template>
+
+<script>
+import sourceData from "@/data.json";
+
+export default {
+  props: {
+    posts: {
+      type: Array,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      users: sourceData.users,
+    };
+  },
+  methods: {
+    userById(userId) {
+      return this.users.find((u) => u.id === userId);
+    },
+  },
+};
+</script>
--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -2,30 +2,14 @@
   <div>Hello Thread</div>
   <div class="flex flex-col w-full border-opacity-50">
     <h2>{{ thread.title }}</h2>
-    <div
-      class="card bg-base-300 rounded-box"
-      v-for="postId in thread.posts"
-      :key="postId"
-    >
-      <div class="w-10 rounded-full">
-        <img :src="userById(postById(postId).userId).avatar" alt="user" />
-      </div>
-      <div class="card-body">
-        <p>
-          {{ userById(postById(postId).userId).name }}
-        </p>
-        <p>{{ postById(postId).text }}</p>
-        <p>post count: {{ userById(postById(postId).userId).postsCount }}</p>
-        <p>publish at: {{ postById(postId).publishedAt }}</p>
-      </div>
-      <div class="divider divider-accent"></div>
-    </div>
+    <post-list :posts="threadPosts" />
     <div class="divider"></div>
   </div>
 </template>
 
 <script>
 import sourceData from "@/data.json";
+import PostList from "@/components/PostList.vue";
 
 export default {
   props: {
@@ -34,10 +18,12 @@ export default {
       required: true,
     },
   },
+  components: {
+    PostList,
+  },
   data() {
     return {
       threads: sourceData.threads,
-      users: sourceData.users,
       posts: sourceData.posts,
     };
   },
@@ -45,13 +31,8 @@ export default {
     thread() {
       return this.threads.find((t) => t.id === this.id); // also available as this.$route.params.id
     },
-  },
-  methods: {
-    postById(postId) {
-      return this.posts.find((p) => p.id === postId);
-    },
-    userById(userId) {
-      return this.users.find((u) => u.id === userId);
+    threadPosts() {
+      return this.posts.filter((p) => p.threadId === this.id);
     },
   },
 };
