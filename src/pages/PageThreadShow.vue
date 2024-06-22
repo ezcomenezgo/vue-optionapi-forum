9cbb1e8f76f6e462353d6334f004f27dee7514a5
Claire Li <s3310507057@gmail.com>
2024-06-22 23:34:33 +0200

    feat: create a post editor

--- src/pages/PageThreadShow.vue
+++ src/pages/PageThreadShow.vue
@@ -4,6 +4,10 @@
     <h2>{{ thread.title }}</h2>
     <post-list :posts="threadPosts" />
     <div class="divider"></div>
+    <form @submit.prevent="addPost">
+      <textarea v-model="newPostText" name="" id=""></textarea>
+      <button>submit</button>
+    </form>
   </div>
 </template>
 
@@ -25,6 +29,7 @@ export default {
     return {
       threads: sourceData.threads,
       posts: sourceData.posts,
+      newPostText: "",
     };
   },
   computed: {
@@ -33,6 +38,22 @@ export default {
     },
     threadPosts() {
       return this.posts.filter((p) => p.threadId === this.id);
+    },
+  },
+  methods: {
+    addPost() {
+      const postId = "abcd" + Math.floor(Date.now() / 1000);
+      const post = {
+        id: postId,
+        text: this.newPostText,
+        publishedAt: Math.floor(Date.now() / 1000),
+        threadId: this.id,
+        userId: "L664y3qZSubDbT1R6npC0EEybJ73",
+      };
+      // sourceData.posts.push(post);
+      this.posts.push(post);
+      this.thread.posts.push(post.id);
+      this.newPostText = "";
     },
   },
 };
