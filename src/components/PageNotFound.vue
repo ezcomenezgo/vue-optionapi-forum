92cffee25ed60f2eec55f852b883197761592c10
Claire Li <s3310507057@gmail.com>
2024-06-22 17:30:30 +0200

    feat: add 404 not found and router guard of single thread page

---
+++ src/components/PageNotFound.vue
@@ -0,0 +1,4 @@
+<template>
+  <div>404 Not Found</div>
+  <router-link :to="{ name: 'Home' }"> Back to read some threads </router-link>
+</template>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
 import { createWebHistory, createRouter } from "vue-router";
+import sourceData from "@/data.json";
 
 import PageHome from "@/components/PageHome.vue";
 import PageThreadShow from "@/components/PageThreadShow.vue";
+import PageNotFound from "@/components/PageNotFound.vue";
 
 const routes = [
   { path: "/", name: "Home", component: PageHome },
@@ -10,8 +12,27 @@ const routes = [
     name: "ThreadShow",
     component: PageThreadShow,
     props: true,
+    beforeEnter: (to, from, next) => {
+      // ...
+      const threadExist = sourceData.threads.find((t) => t.id === to.params.id);
+      if (threadExist) return next();
+      else
+        next({
+          name: "NotFound",
+          // preserve current path and remove the first char to avoid the target URL starting with `//`
+          params: { pathMatch: to.path.substring(1).split("/") },
+          // preserve existing query and hash if any
+          query: to.query,
+          hash: to.hash,
+        });
+    },
   },
-  // { path: '/about', component: AboutView },
+  // this is a soft 404, because it just deal with wrong url on client side
+  {
+    path: "/:pathMatch(.*)*",
+    name: "NotFound",
+    component: PageNotFound,
+  },
 ];
 
 const router = createRouter({
