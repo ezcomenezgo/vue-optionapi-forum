01eb155515d21deec50db7609d38d274073bd8ba
Claire Li <s3310507057@gmail.com>
2024-06-23 18:58:23 +0200

    feat: change sourceData using mutation and action in vuex

--- src/components/PostEditor.vue
+++ src/components/PostEditor.vue
@@ -16,9 +16,7 @@ export default {
   },
   methods: {
     save() {
-      const postId = "abcd" + Math.floor(Date.now() / 1000);
       const post = {
-        id: postId,
         text: this.text,
         publishedAt: Math.floor(Date.now() / 1000),
         threadId: this.id,
--- src/store/index.js
+++ src/store/index.js
@@ -7,6 +7,27 @@ const store = createStore({
       sourceData,
     };
   },
+  mutations: {
+    setPost(state, { post }) {
+      state.sourceData.posts.push(post);
+    },
+    appendPostToThread(state, { postId, threadId }) {
+      const thread = state.sourceData.threads.find(
+        (thread) => thread.id === threadId
+      );
+      thread.posts.push(postId);
+    },
+  },
+  actions: {
+    createPost(context, post) {
+      post.id = "abcd" + Math.floor(Date.now() / 1000);
+      context.commit("setPost", { post }); // set the post
+      context.commit("appendPostToThread", {
+        postId: post.id,
+        threadId: post.threadId,
+      }); // append post to thread
+    },
+  },
 });
 
 export default store;
